4bab23a6a6664cc2d421a7760b1b0a894b014940
Johnny <joaogbdesouza@gmail.com>
2024-01-07 17:13:32 -0300

    método no repository pra filtrar despesas por mês

--- pkg/infrastructure/repositories/expenseRepository.go
+++ pkg/infrastructure/repositories/expenseRepository.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"time"
 
+	dtos "github.com/gabszero/expenses-api/pkg/application/Dtos"
 	"github.com/gabszero/expenses-api/pkg/infrastructure/models"
 )
 
@@ -22,6 +23,31 @@ func (etr *ExpenseRepository) GetAll(filter models.Expense) []models.Expense {
 		endingDate := filter.Date.Add(24*time.Hour - 1).Format("2006-01-02 15:04:05")
 
 		query.Where("date BETWEEN ? and ?", startingDate, endingDate)
+	}
+
+	query.Find(&result)
+
+	return result
+}
+
+func (etr *ExpenseRepository) GetMonthsByMonth(filter dtos.FilterExpenseMonthDto) []models.Expense {
+	result := []models.Expense{}
+	if db == nil {
+		panic("no db instance found")
+	}
+
+	query := db.Model(&result).Preload("ExpenseType")
+	if !filter.Date.IsZero() {
+		startingDate := filter.Date.Format("2006-01-02 15:04:05")
+		tempDate := filter.Date.AddDate(0, 1, -filter.Date.Day())
+		tempDate = tempDate.Add(time.Hour * 23)
+		tempDate = tempDate.Add(time.Minute * 59)
+		tempDate = tempDate.Add(time.Second * 59)
+
+		endingDate := tempDate.Format("2006-01-02 15:04:05")
+
+		query.Where("date BETWEEN ? and ?", startingDate, endingDate)
+
 	}
 
 	query.Find(&result)
