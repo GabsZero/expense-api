8c4958300118dd34abb3b3cd41809c4b17f6cd14
Johnny <joaogbdesouza@gmail.com>
2024-01-07 17:13:46 -0300

    método no service pra filtrar despesas por mês

--- pkg/domain/services/expensesService.go
+++ pkg/domain/services/expensesService.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	dtos "github.com/gabszero/expenses-api/pkg/application/Dtos"
 	"github.com/gabszero/expenses-api/pkg/infrastructure/models"
 	"github.com/gabszero/expenses-api/pkg/infrastructure/repositories"
 )
@@ -11,4 +12,8 @@ type ExpenseService struct {
 
 func (ets *ExpenseService) GetExpenses(filter models.Expense) []models.Expense {
 	return ets.ExpenseRepository.GetAll(filter)
+}
+
+func (ets *ExpenseService) FilterExpensesInAMonth(filter dtos.FilterExpenseMonthDto) []models.Expense {
+	return ets.ExpenseRepository.GetMonthsByMonth(filter)
 }
