87c3e9a7af5bb06623ef82360c7aba82ee060277
Johnny <joaogbdesouza@gmail.com>
2024-01-10 00:08:39 -0300

    rota pra gravar nova despesa

--- pkg/application/routes/expenses/index.go
+++ pkg/application/routes/expenses/index.go
@@ -11,5 +11,6 @@ type ExpensesRoutes struct {
 func (er *ExpensesRoutes) DefineRoutes(router *gin.Engine) {
 	expenseController := controllers.ExpenseController{}
 	router.GET("/expenses", expenseController.Index)
+	router.POST("/expenses", expenseController.Store)
 	router.GET("/getExpensesByMonth", expenseController.FindExpensesInAMonth)
 }
