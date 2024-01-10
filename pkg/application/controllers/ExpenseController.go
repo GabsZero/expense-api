b1772dcc045d4dd26518556290e23415d6213a64
Johnny <joaogbdesouza@gmail.com>
2024-01-10 00:28:14 -0300

    terminando o controller de salvar despesa

--- pkg/application/controllers/ExpenseController.go
+++ pkg/application/controllers/ExpenseController.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"fmt"
+
 	dtos "github.com/gabszero/expenses-api/pkg/application/Dtos"
 	"github.com/gabszero/expenses-api/pkg/domain/services"
 	"github.com/gabszero/expenses-api/pkg/infrastructure/models"
@@ -33,13 +35,20 @@ func (etc *ExpenseController) Store(context *gin.Context) {
 	newExpense := dtos.StoreExpense{}
 
 	err := context.Bind(&newExpense)
+
 	if err != nil {
 		context.JSON(400, gin.H{
 			"message": err.Error(),
 		})
-
 		return
 	}
+
+	fmt.Println(newExpense)
+	expense := etc.ExpenseService.StoreExpense(newExpense)
+
+	context.JSON(200, gin.H{
+		"expense": expense,
+	})
 }
 
 func (etc *ExpenseController) FindExpensesInAMonth(context *gin.Context) {
