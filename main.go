3da5e71d247d09040547bdf4daafc3d9d5853f05
Johnny <joaogbdesouza@gmail.com>
2024-01-11 22:50:28 -0300

    setando timezone pros br hue

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gabszero/expenses-api/pkg/application/routes"
 	"github.com/gabszero/expenses-api/pkg/infrastructure/repositories"
 	"github.com/gabszero/expenses-api/pkg/infrastructure/seeders"
 )
 
 func main() {
-
+	// setando o horário local
+	time.Local, _ = time.LoadLocation("America/Sao_Paulo")
 	repository := repositories.Repository{}
 	repository.StartDabase()
 
