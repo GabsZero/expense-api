13f48b3c68347a3f1e8f1e182d458b02afdbb300
Johnny <joaogbdesouza@gmail.com>
2024-01-17 23:29:51 -0300

    ajustando bind do dto de gravar entrada

--- pkg/application/Dtos/StoreIncome.go
+++ pkg/application/Dtos/StoreIncome.go
@@ -3,6 +3,6 @@ package dtos
 type StoreIncome struct {
 	Name         string  `form:"name"  binding:"required"`
 	Date         string  `form:"date" binding:"required"`
-	IncomeTypeId uint    `form:"expenseTypeId"  binding:"required"`
+	IncomeTypeId uint    `form:"incomeTypeId"  binding:"required"`
 	Amount       float32 `form:"amount"  binding:"required"`
 }
